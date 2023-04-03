e8f36e84b34a85f621827ddeb6ebece16fed4935
Luka Korica 81/2022 RN <lkorica8122rn@raf.rs>
2023-04-04 01:32:20 +0200

    Small refactoring

--- user/decr.c
+++ user/decr.c
@@ -6,8 +6,7 @@
 
 char buf[512];
 
-void decr_usage(void)
-{
+void decr_usage(void) {
     printf("Usage: decr [OPTIONS] [FILE]...\n"
             "Decrypt one or more files using Caesar cipher.\n"
             "Options:\n"
@@ -15,18 +14,15 @@ void decr_usage(void)
             "  -a, --decrypt-all: Decrypt all encrypted files in the working directory\n");
 }
 
-void decrypt_file(char *path)
-{
+void decrypt_file(char *path) {
     int fd;
 
-    if ((fd = open(path, O_RDWR)) < 0)
-    {
+    if ((fd = open(path, O_RDWR)) < 0) {
         printf("decr: cannot open %s\n", path);
         return;
     }
 
-    if (decr(fd) < 0)
-    {
+    if (decr(fd) < 0) {
         printf("decr: failed to decrypt %s\n", path);
         close(fd);
         return;
@@ -41,18 +37,15 @@ void decrypt_all(void)
     struct dirent de;
     struct stat st;
 
-    if ((fd = open(".", O_RDONLY)) < 0)
-    {
+    if ((fd = open(".", O_RDONLY)) < 0) {
         printf("decr: cannot open .\n");
         return;
     }
 
-    while (read(fd, &de, sizeof(de)) == sizeof(de))
-    {
+    while (read(fd, &de, sizeof(de)) == sizeof(de)) {
         if (de.inum == 0 || de.name[0] == '.')
             continue;
-        if (stat(de.name, &st) < 0)
-        {
+        if (stat(de.name, &st) < 0) {
             printf("decr: cannot stat %s\n", de.name);
             continue;
         }
@@ -69,43 +62,35 @@ int main(int argc, char *argv[])
     int i;
     int decrypt_all_flag = 0;
 
-    for (i = 1; i < argc; i++)
-    {
-        if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0)
-        {
+    for (i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0) {
             decr_usage();
             exit();
         }
-        else if (strcmp(argv[i], "-a") == 0 || strcmp(argv[i], "--decrypt-all") == 0)
-        {
+        else if (strcmp(argv[i], "-a") == 0 || strcmp(argv[i], "--decrypt-all") == 0) {
             decrypt_all_flag = 1;
         }
-        else if (argv[i][0] == '-')
-        {
+        else if (argv[i][0] == '-') {
             printf("decr: unknown option %s\n", argv[i]);
             decr_usage();
             exit();
         }
-        else
-        {
+        else {
             break;
         }
     }
 
-    if (i == argc && !decrypt_all_flag)
-    {
+    if (i == argc && !decrypt_all_flag) {
         decr_usage();
         exit();
     }
 
-    if (decrypt_all_flag)
-    {
+    if (decrypt_all_flag) {
         decrypt_all();
         exit();
     }
 
-    for (; i < argc; i++)
-    {
+    for (; i < argc; i++) {
         decrypt_file(argv[i]);
     }
 
--- user/encr.c
+++ user/encr.c
@@ -6,8 +6,7 @@
 
 char buf[512];
 
-void encr_usage(void)
-{
+void encr_usage(void) {
     printf("Usage: encr [OPTIONS] [FILE]...\n"
             "Encrypt one or more files using Caesar cipher.\n"
             "Options:\n"
@@ -15,18 +14,15 @@ void encr_usage(void)
             "  -a, --encrypt-all: Encrypt all non-encrypted files in the working directory\n");
 }
 
-void encrypt_file(char *path)
-{
+void encrypt_file(char *path) {
     int fd;
 
-    if ((fd = open(path, O_RDWR)) < 0)
-    {
+    if ((fd = open(path, O_RDWR)) < 0) {
         printf("encr: cannot open %s\n", path);
         return;
     }
 
-    if (encr(fd) < 0)
-    {
+    if (encr(fd) < 0) {
         printf("encr: failed to encrypt %s\n", path);
         close(fd);
         return;
@@ -35,24 +31,20 @@ void encrypt_file(char *path)
     close(fd);
 }
 
-void encrypt_all(void)
-{
+void encrypt_all(void) {
     int fd;
     struct dirent de;
     struct stat st;
 
-    if ((fd = open(".", O_RDONLY)) < 0)
-    {
+    if ((fd = open(".", O_RDONLY)) < 0) {
         printf("encr: cannot open .\n");
         return;
     }
 
-    while (read(fd, &de, sizeof(de)) == sizeof(de))
-    {
+    while (read(fd, &de, sizeof(de)) == sizeof(de)) {
         if (de.inum == 0 || de.name[0] == '.')
             continue;
-        if (stat(de.name, &st) < 0)
-        {
+        if (stat(de.name, &st) < 0) {
             printf("encr: cannot stat %s\n", de.name);
             continue;
         }
@@ -64,48 +56,39 @@ void encrypt_all(void)
     close(fd);
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     int i;
     int encrypt_all_flag = 0;
 
-    for (i = 1; i < argc; i++)
-    {
-        if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0)
-        {
+    for (i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0) {
             encr_usage();
             exit();
         }
-        else if (strcmp(argv[i], "-a") == 0 || strcmp(argv[i], "--encrypt-all") == 0)
-        {
+        else if (strcmp(argv[i], "-a") == 0 || strcmp(argv[i], "--encrypt-all") == 0) {
             encrypt_all_flag = 1;
         }
-        else if (argv[i][0] == '-')
-        {
+        else if (argv[i][0] == '-') {
             printf("encr: unknown option %s\n", argv[i]);
             encr_usage();
             exit();
         }
-        else
-        {
+        else {
             break;
         }
     }
 
-    if (i == argc && !encrypt_all_flag)
-    {
+    if (i == argc && !encrypt_all_flag) {
         encr_usage();
         exit();
     }
 
-    if (encrypt_all_flag)
-    {
+    if (encrypt_all_flag) {
         encrypt_all();
         exit();
     }
 
-    for (; i < argc; i++)
-    {
+    for (; i < argc; i++) {
         encrypt_file(argv[i]);
     }
 
